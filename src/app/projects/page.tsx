5880bf02dba25ba9100397ce21b54215b50697d3
Stephen Haskins <haskinss@tcd.ie>
2024-10-24 18:04:43 +0100

    Added project description

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import Breadcrumb from "@/components/Breadcrumb";
+import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
 
 interface Project {
   id: number;
@@ -8,32 +9,30 @@ interface Project {
   description: string;
   imageUrl: string;
   category: "Hackathon" | "Personal" | "Group" | "College";
-  date: string; // Add date field
+  date: string;
 }
 
 const projects: Project[] = [
   {
     id: 1,
     title: "BorderBuddy AI",
-    description: "Placeholder description",
-    imageUrl: "/images/project1.jpg",
+    description:
+      "Our team recently won the Public Interest Award at HackTrinity, a hackathon hosted by Trinity College Dublin. Competing under the theme of Generative AI x Law, we developed BorderBuddy AI, a web application designed to simplify visa and immigration law for travelers. Built in under 10 hours, our project helps users easily understand the legal implications of international travel using AI-driven analysis. Click to learn more. ",
+    imageUrl: "/images/icon.jpg",
     category: "Hackathon",
-    date: "January 2024", // Example date
+    date: "January 2024",
   },
   {
     id: 2,
-    title: "ScoreMySkill",
+    title: "Future Project",
     description: "This project is still in development. More to come soon!",
-    imageUrl: "/images/project2.jpg",
+    imageUrl: "/images/icon2.png",
     category: "Group",
-    date: "January 2024", // Example date
+    date: "January 2024",
   },
- 
-
   // Add more projects as needed
 ];
 
-// Function to filter projects by category
 const filterProjectsByCategory = (category: Project["category"]) => {
   return projects.filter((project) => project.category === category);
 };
@@ -44,19 +43,21 @@ const ProjectsPage: React.FC = () => {
   return (
     <div className="bg-zinc-50 min-h-screen">
       <div className="container mx-auto px-10 py-8 flex flex-col">
-        <div className="mt-auto">
-          <Breadcrumb items={breadcrumbItems} />
-        </div>
-        <div className="bg-white rounded-lg shadow-md p-8 mb-12 mt-12">
-          <h1 className="text-4xl font-bold mb-4 text-left">My Projects</h1>
-          <p className="text-gray-600 text-lg text-left">
-            This page contains a collection of projects I&apos;ve worked on.
-            Each project is categorized into one of the following categories:
-            Hackathon, Personal, Group, and College. Click on a project to learn
-            more.
-          </p>
-        </div>
-        <div className="space-y-16">
+        <Breadcrumb items={breadcrumbItems} />
+        <Card className="mb-8 mt-8">
+          <CardHeader>
+            <CardTitle className="text-3xl font-bold">My Projects</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <p className="text-gray-600 text-lg">
+              This page contains a collection of projects I&apos;ve worked on.
+              Click on a project&apos;s card to learn more.
+            </p>
+          </CardContent>
+        </Card>
+      </div>
+      <div className="container mx-auto px-20 py-8 flex flex-col">
+        <div className="space-y-10">
           {["Hackathon", "Personal", "Group", "College"].map((category) => (
             <div key={category}>
               {filterProjectsByCategory(category as Project["category"]).map(
@@ -75,21 +76,23 @@ const ProjectsPage: React.FC = () => {
                             {project.category} Project
                           </p>
                         </div>
-                        <span className="text-gray-500 mr-3 mt-3.5">{project.date}</span>
+                        <span className="text-gray-500 mt-3.5 mr-3 text-1xl">
+                          {project.date}
+                        </span>
                       </div>
                     </div>
                     <div className="flex">
                       <div className="w-3/4 p-4">
-                        <p className="text-gray-600 text-lg">
+                        <p className="text-gray-600 text-lg text-left ml-5 mt-5">
                           {project.description}
                         </p>
                       </div>
-                      <div className="w-1/4">
+                      <div className="w-1/6 mt-5 mb-5 mr-5 mx-auto">
                         <Image
                           src={project.imageUrl}
                           alt={project.title}
-                          width={600}
-                          height={400}
+                          width={200}
+                          height={200}
                           className="h-full w-full object-cover"
                         />
                       </div>
